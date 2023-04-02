28d8613b229911aa8f9b39d641d03f6300f2325b
L.B <mrlawrencelane@gmail.com>
2023-04-02 10:50:37 -0400

    checkpoint

--- src/routes/api/v1/player/[id]/+server.ts
+++ src/routes/api/v1/player/[id]/+server.ts
@@ -1,30 +1,49 @@
 
 import {generateAlias} from '$lib/utils';
 import type { RequestHandler } from './$types';
-import { json, error } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({
-  locals: { supabase, getSession },
-  params
-}) => {
-  const player_id = params.id;
-  console.log('player_id', player_id)
-  const session = await getSession();
+export const GET: RequestHandler = async ({ locals: { supabase, getSession }, params }) => {
+	const player_id = params.id;
+	console.log('player_id', player_id);
 
-  if (!session) {
-    // the user is not signed in
-    throw error(401, { message: 'Unauthorized' });
-  }
-  
-  const { data, error } = await supabase
+	const session = await getSession();
+
+	if (!session) {
+		// the user is not signed in
+		throw error(401, { message: 'Unauthorized' });
+	}
+
+	// Query the database for the player with the given ID
+	const { data: existingData, error: existingError } = await supabase
 		.from('players')
 		.select('*')
 		.eq('player_id', player_id)
 		.single();
 
-  console.log("player data", data)
+	if (existingError) {
+		throw existingError;
+	}
 
-  return new Response(JSON.stringify({ ...data }));
+	if (!existingData) {
+		// If no player with this ID exists, generate a new alias and insert it into the database
+		const alias = generateAlias();
+
+		const { data: newData, error: newError } = await supabase
+			.from('players')
+			.insert({ player_id, alias })
+			.select('*')
+			.single();
+
+		if (newError) {
+			throw newError;
+		}
+
+		return new Response(JSON.stringify(newData));
+	}
+
+	// Return the existing player data
+	return new Response(JSON.stringify(existingData));
 };
 
 
--- src/routes/player/[slug]/+page.svelte
+++ src/routes/player/[slug]/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-
-  export let data
+  import type { Player } from '$lib/types'
+  export let data: { player?: Player };
   console.log("player page ", data)
 
+  let player: Player | undefined = data?.player
 
 </script>
+
+<h1>{player?.alias}</h1>
 
